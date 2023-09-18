3bb85b156f72d46969d40aef5728afc3883c172c
comedeprevilleFD <121291556+comedeprevilleFD@users.noreply.github.com>
2023-09-18 11:22:29 +0200

    Ajout initial des fichiers du site web
    Ajout initial des fichiers du site web

---
+++ script.js
@@ -0,0 +1,23 @@
+// Variables globales pour les codes hexadécimaux
+const colorCodes = ["#122236", "#1BA1DC", "#F8FAFC", "#ce173c", "#383838", "#bababa"];
+
+// Ajoute un gestionnaire d'événements à chaque bouton
+for (let i = 1; i <= colorCodes.length; i++) {
+    const btn = document.getElementById(`btn${i}`);
+    btn.addEventListener('click', () => {
+        // Copie le code hexadécimal dans le presse-papiers
+        const code = colorCodes[i - 1];
+        navigator.clipboard.writeText(code).then(function () {
+            // Change le texte et la couleur du bouton pendant une seconde
+            btn.textContent = "✓ Copied!";
+            btn.style.backgroundColor = "#02bd23"; // Couleur de fond verte
+            setTimeout(() => {
+                // Revert to the original state after 1 second
+                btn.textContent = code;
+                btn.style.backgroundColor = code;
+            }, 1000);
+        }).catch(function (err) {
+            console.error('Impossible de copier le texte : ', err);
+        });
+    });
+}
